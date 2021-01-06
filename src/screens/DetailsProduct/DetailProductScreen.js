628d6478d24664fad88954d3371c902711a65903
benz45 <benz45.th@outlook.com>
2021-01-06 13:40:06 +0700

    Add scrollView on detail product screen

--- src/screens/DetailsProduct/DetailProductScreen.js
+++ src/screens/DetailsProduct/DetailProductScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScrollView} from 'react-native';
 
 // Paper
 import {List} from 'react-native-paper';
@@ -21,63 +22,67 @@ const DetailProductScreen = () => {
 
   return (
     <React.Fragment>
-      {_keyList.map((elem) => (
-        <React.Fragment key={elem.key}>
-          <Styled.ContainerAvatarImage>
-            <Styled.AvatarImage
-              source={{
-                uri: elem.image,
-              }}
-            />
-          </Styled.ContainerAvatarImage>
-          <Styled.ContainerInfo>
-            <Styled.Title>{elem.name}</Styled.Title>
-            <Styled.ContainerBtn>
-              <Styled.BtnHistory
-                onPress={() => navigate('Stack_History', elem.key)}>
-                History
-              </Styled.BtnHistory>
-              {elem.createdBy == _displayName && (
-                <Styled.BtnEdit
-                  onPress={() => navigate('DetailProductEditScreen')}>
-                  Edit Product
-                </Styled.BtnEdit>
-              )}
-            </Styled.ContainerBtn>
-            <List.Section>
-              <List.Subheader style={{color: accent}}>Info</List.Subheader>
-              <List.Item
-                title="Description"
-                description={elem.description}
-                left={() => <List.Icon icon="card-bulleted" />}
+      <ScrollView>
+        {_keyList.map((elem) => (
+          <React.Fragment key={elem.key}>
+            <Styled.ContainerAvatarImage>
+              <Styled.AvatarImage
+                source={{
+                  uri: elem.image,
+                }}
               />
-              <List.Item
-                title="Product key"
-                description={elem.key}
-                left={() => <List.Icon icon="key" />}
-              />
-              <List.Item
-                title="Created by"
-                description={elem.createdBy}
-                left={() => <List.Icon icon="account-star" />}
-              />
-            </List.Section>
-            <List.Section>
-              <List.Subheader style={{color: accent}}>Date time</List.Subheader>
-              <List.Item
-                title="Created On"
-                description={elem.createdDate}
-                left={() => <List.Icon icon="calendar-import" />}
-              />
-              <List.Item
-                title="Latest status"
-                description={elem.latestStatus}
-                left={() => <List.Icon icon="update" />}
-              />
-            </List.Section>
-          </Styled.ContainerInfo>
-        </React.Fragment>
-      ))}
+            </Styled.ContainerAvatarImage>
+            <Styled.ContainerInfo>
+              <Styled.Title>{elem.name}</Styled.Title>
+              <Styled.ContainerBtn>
+                <Styled.BtnHistory
+                  onPress={() => navigate('Stack_History', elem.key)}>
+                  History
+                </Styled.BtnHistory>
+                {elem.createdBy == _displayName && (
+                  <Styled.BtnEdit
+                    onPress={() => navigate('DetailProductEditScreen')}>
+                    Edit Product
+                  </Styled.BtnEdit>
+                )}
+              </Styled.ContainerBtn>
+              <List.Section>
+                <List.Subheader style={{color: accent}}>Info</List.Subheader>
+                <List.Item
+                  title="Description"
+                  description={elem.description}
+                  left={() => <List.Icon icon="card-bulleted" />}
+                />
+                <List.Item
+                  title="Product key"
+                  description={elem.key}
+                  left={() => <List.Icon icon="key" />}
+                />
+                <List.Item
+                  title="Created by"
+                  description={elem.createdBy}
+                  left={() => <List.Icon icon="account-star" />}
+                />
+              </List.Section>
+              <List.Section>
+                <List.Subheader style={{color: accent}}>
+                  Date time
+                </List.Subheader>
+                <List.Item
+                  title="Created On"
+                  description={elem.createdDate}
+                  left={() => <List.Icon icon="calendar-import" />}
+                />
+                <List.Item
+                  title="Latest status"
+                  description={elem.latestStatus}
+                  left={() => <List.Icon icon="update" />}
+                />
+              </List.Section>
+            </Styled.ContainerInfo>
+          </React.Fragment>
+        ))}
+      </ScrollView>
     </React.Fragment>
   );
 };
